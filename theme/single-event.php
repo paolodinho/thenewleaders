4ad202900dc0491c8d67bad7db06217765fc0410
Do Hieu <hieudx3107@gmail.com>
2026-06-05 10:11:01 +0700

    auto-update 2026-06-05 10:11

--- theme/single-event.php
+++ theme/single-event.php
@@ -8,6 +8,13 @@ while (have_posts()) : the_post();
     $loc  = get_post_meta($id, '_tnl_event_location', true);
     $reg  = get_post_meta($id, '_tnl_event_register', true);
     $title = function_exists('tnl_event_title') ? tnl_event_title($id) : get_the_title();
+    if ($vi) {
+        $date_vi = get_post_meta($id, '_tnl_event_date_vi', true);
+        $loc_vi  = get_post_meta($id, '_tnl_event_location_vi', true);
+        if ($date_vi) { $date = $date_vi; }
+        if ($loc_vi)  { $loc  = $loc_vi; }
+    }
+    $body_vi = $vi ? get_post_meta($id, '_tnl_event_body_vi', true) : '';
 ?>
 <main class="site-main page-event-single">
   <article class="ev-single">
@@ -28,7 +35,10 @@ while (have_posts()) : the_post();
         <figure class="ev-single__fig"><?php the_post_thumbnail('large'); ?></figure>
       <?php endif; ?>
 
-      <div class="ev-single__body"><?php the_content(); ?></div>
+      <div class="ev-single__body"><?php
+        if ($body_vi) { echo wp_kses_post($body_vi); }
+        else { the_content(); }
+      ?></div>
 
       <?php if ($reg) : ?>
         <a href="<?php echo esc_url($reg); ?>" target="_blank" rel="noopener noreferrer" class="btn btn--primary ev-single__reg"><?php echo esc_html($vi ? 'Đăng ký tham gia' : 'Register now'); ?></a>
