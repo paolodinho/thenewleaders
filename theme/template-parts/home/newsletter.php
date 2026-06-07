cfde8e28037b52ad261961e5b64ea14328ccbff2
Do Hieu <hieudx3107@gmail.com>
2026-06-07 07:25:36 +0700

    auto-update 2026-06-07 07:25

--- theme/template-parts/home/newsletter.php
+++ theme/template-parts/home/newsletter.php
@@ -2,6 +2,11 @@
   <div class="container">
     <div class="newsletter__inner">
       <p class="newsletter__title"><?php echo tnl_t('nl_title'); ?></p>
+      <!-- Decorative hand-drawn arrow (matches live site) -->
+      <svg class="newsletter__curly-arrow" width="80" height="90" viewBox="0 0 80 90" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+        <path d="M10 10 C 30 5, 70 20, 68 45 C 66 65, 40 70, 42 82" stroke="white" stroke-width="2" stroke-linecap="round" fill="none"/>
+        <path d="M36 78 L 42 84 L 50 78" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" fill="none"/>
+      </svg>
       <form class="newsletter__form tnl-ajax-form" action="#" method="post" data-form-type="Newsletter (Home)">
         <div class="newsletter__row">
           <label class="newsletter__label" for="nl-name"><?php echo tnl_t('nl_name'); ?></label>
