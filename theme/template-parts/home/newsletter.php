b7a943bd0bd81603a178f56ce12df878c7825a1f
Do Hieu <hieudx3107@gmail.com>
2026-06-07 08:50:41 +0700

    auto-update 2026-06-07 08:50

--- theme/template-parts/home/newsletter.php
+++ theme/template-parts/home/newsletter.php
@@ -1,13 +1,16 @@
 <section class="newsletter" id="newsletter">
   <div class="container">
     <div class="newsletter__inner">
-      <p class="newsletter__title"><?php echo tnl_t('nl_title'); ?></p>
-      <!-- Decorative hand-drawn arrow (matches live site) -->
-      <svg class="newsletter__curly-arrow" width="80" height="90" viewBox="0 0 80 90" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-        <path d="M10 10 C 30 5, 70 20, 68 45 C 66 65, 40 70, 42 82" stroke="white" stroke-width="2" stroke-linecap="round" fill="none"/>
-        <path d="M36 78 L 42 84 L 50 78" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" fill="none"/>
-      </svg>
+      <div class="newsletter__header">
+        <p class="newsletter__title"><?php echo tnl_t('nl_title'); ?></p>
+        <!-- Decorative hand-drawn looping arrow (matches live site) -->
+        <svg class="newsletter__curly-arrow" width="90" height="115" viewBox="0 0 90 115" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+          <path d="M52 18 C72 5, 82 35, 68 52 C54 68, 28 62, 22 44 C16 26, 32 10, 52 22 C54 24, 55 30, 52 45 C48 65, 40 85, 34 98" stroke="white" stroke-width="2.2" stroke-linecap="round" fill="none"/>
+          <path d="M26 93 L 34 100 L 43 93" stroke="white" stroke-width="2.2" stroke-linecap="round" stroke-linejoin="round" fill="none"/>
+        </svg>
+      </div>
       <form class="newsletter__form tnl-ajax-form" action="#" method="post" data-form-type="Newsletter (Home)">
+
         <div class="newsletter__row">
           <label class="newsletter__label" for="nl-name"><?php echo tnl_t('nl_name'); ?></label>
           <input type="text" id="nl-name" name="name" required class="newsletter__input">
